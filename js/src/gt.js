929c0b58f2633ff48452db6822a2e86ffc6c70f4
Aysegul Yonet <aysegulyonet@gmail.com>
2013-09-24 22:54:20 -0700

    initial sphere geometry

--- js/src/gt.js
+++ js/src/gt.js
@@ -1,9 +1,57 @@
-var gt = {
-  init: function () {
-  	var renderer = gt.Renderer();
-  	var scene = gt.Scene();
-  	var camera = gt.Camera();
+var gt = {};
+  
 
-  	renderer.render(scene, camera);
+gt.Globe = function (container) {
+  var Shaders = {
+	'earth': {}
+  };
+  var camera, scene, renderer;
+  var w = window.innerWidth,
+      h = window.innerHeight;
+
+  function init () {
+	
+	setScene();
+	setCamera();
+	setRenderer();
+
+	render();
+  };
+
+  function setRenderer () {
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( w, h );
+	container.appendChild( renderer.domElement );
+  };
+
+  function setScene () {
+
+	scene = new THREE.Scene();
+	addMesh();
+  };
+
+  function setCamera () {
+    camera = new THREE.PerspectiveCamera( 75, w / h, 0.1, 1000 );
+    //camera.lookAt(scene.position);
+    camera.position.z = 5;
+    scene.add(camera);
+  };
+
+  function addMesh () {
+  	//var earthmap = "../images/earth_surface_2048.jpg";
+  	var geometry = new THREE.SphereGeometry(1, 32, 32);
+  	var material = new THREE.MeshBasicMaterial( { color: 0x00CCFF } );
+	var globe = new THREE.Mesh(geometry, material);
+
+	scene.add(globe);
+  };
+
+  function render() {
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
   }
+
+  init();
+  
 };
