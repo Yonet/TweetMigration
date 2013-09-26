42c42a492752c3c39f4037fc7acc2620a984d81a
Aysegul Yonet <aysegulyonet@gmail.com>
2013-09-25 22:31:06 -0700

    added utils function to calculate the coordinates on the sphere

---
+++ js/src/gt.utils.js
@@ -0,0 +1,11 @@
+// convert the positions from a lat, lon to a position on a sphere.
+function latLongToVector3(lat, lon, radius, heigth) {
+    var phi = (lat)*Math.PI/180;
+    var theta = (lon-180)*Math.PI/180;
+
+    var x = -(radius+heigth) * Math.cos(phi) * Math.cos(theta);
+    var y = (radius+heigth) * Math.sin(phi);
+    var z = (radius+heigth) * Math.cos(phi) * Math.sin(theta);
+
+    return new THREE.Vector3(x,y,z);
+}
