3d16e4988179fc2c77011aab1a6f35da98d936b0
Aysegul Yonet <aysegulyonet@gmail.com>
2013-09-26 02:40:11 -0700

    implemented particle system

--- js/src/gt.js
+++ js/src/gt.js
@@ -1,12 +1,9 @@
 var gt = {};
-  
+
 
 gt.Globe = function (container) {
-  var Shaders = {
-	'earth': {}
-  };
-  
-  var camera, scene, light, renderer, controls, earth;
+  var data = ['41.0136', '28.9550', '200', '30'];
+  var camera, scene, light, renderer, controls, earth,tweets;
   var w = window.innerWidth,
       h = window.innerHeight;
 
@@ -16,6 +13,9 @@ gt.Globe = function (container) {
   	setCamera();
   	setRenderer();
     addController();
+    tweets = gt.utils.addPoints(data);
+    scene.add(tweets);
+    //addSkybox();
   	render();
 
   };
@@ -29,7 +29,7 @@ gt.Globe = function (container) {
     stats.domElement.style.top = '0px';
     stats.domElement.style.zIndex = 100;
   	container.appendChild( renderer.domElement );
-    container.appendChild( stats.domElement );
+    //container.appendChild( stats.domElement );
 
   };
 
@@ -45,7 +45,7 @@ gt.Globe = function (container) {
     light = new THREE.DirectionalLight( 0x002288 );
     light.position.set( -1, -1 , -1);
     scene.add( light );
-    light = new THREE.AmbientLight( 0x222222, 5);
+    light = new THREE.AmbientLight( 0x222222, 10);
   	scene.add( light );
   	addMesh();
 
@@ -53,7 +53,7 @@ gt.Globe = function (container) {
 
   function setCamera () {
 
-    camera = new THREE.PerspectiveCamera( 60, w / h, 0.1, 2000 );
+    camera = new THREE.PerspectiveCamera( 60, w / h, 0.1, 20000 );
     camera.position.z = 500;
     scene.add(camera);
 
@@ -67,7 +67,7 @@ gt.Globe = function (container) {
 
   function addMesh () {
 
-    var geometry = new THREE.SphereGeometry(200, 32, 32);
+    var geometry = new THREE.SphereGeometry(200, 40, 30);
     var material = new THREE.MeshPhongMaterial();
     material.map = THREE.ImageUtils.loadTexture('../images/earth_surface_2048.jpg');
     material.bumpMap = THREE.ImageUtils.loadTexture('../images/bump_earth.jpg');
@@ -76,26 +76,51 @@ gt.Globe = function (container) {
 	  var globe = new THREE.Mesh(geometry, material);
     globe.updateMatrix();
     globe.matrixAutoUpdate = false;
-    globe.rotation.z = 0.41;
     var cloudGeometry   = new THREE.SphereGeometry(205, 32, 32);
 	  var material  = new THREE.MeshPhongMaterial({
-	  map     : THREE.ImageUtils.loadTexture('../images/earth_clouds_1024.png'),
-	  side        : THREE.DoubleSide,
-	  opacity     : 0.8,
-	  transparent : true,
-	  depthWrite  : false
+  	  map     : THREE.ImageUtils.loadTexture('../images/earth_clouds_1024.png'),
+  	  side        : THREE.DoubleSide,
+  	  opacity     : 0.8,
+  	  transparent : true,
+  	  depthWrite  : false
 
 	  });
 
   	var cloudMesh = new THREE.Mesh(cloudGeometry, material);
     earth = new THREE.Object3D();
+    // earth.rotation.z = 0.41;
   	earth.add(globe);
   	earth.add(cloudMesh);
   	scene.add(earth);
 
   };
 
-  function onWindowResize() {
+  // function addSkybox () {
+  //     var urlPrefix = "../images/skybox/Purple_Nebula_";
+  //     var urls = [
+  //         urlPrefix + "right1.png", urlPrefix + "left2.png",
+  //         urlPrefix + "top3.png", urlPrefix + "bottom4.png",
+  //         urlPrefix + "front5.png", urlPrefix + "back6.png"
+  //     ];
+
+  //     THREE.ImageUtils.loadTextureCube(urls, {}, function(textureCube) {
+  //         textureCube.format = THREE.RGBFormat;
+  //         var shader = THREE.ShaderLib.cube;
+
+  //         var uniforms = THREE.UniformsUtils.clone( shader.uniforms );
+  //         uniforms.tCube.value = textureCube;
+
+  //         var material = new THREE.ShaderMaterial( {
+  //             fragmentShader: shader.fragmentShader,
+  //             vertexShader: shader.vertexShader,
+  //             uniforms: uniforms,
+  //             side: THREE.BackSide
+  //         } );
+  //         skyboxMesh = new THREE.Mesh( new THREE.CubeGeometry( 1000, 1000, 1000, 1, 1, 1, null, true ), material );
+  //         scene.add( skyboxMesh );
+  //     };}
+
+  function onWindowResize () {
 
     camera.aspect = w / h;
     camera.updateProjectionMatrix();
@@ -106,17 +131,18 @@ gt.Globe = function (container) {
 
   };
 
-  function animate() {
+  function animate () {
 
     requestAnimationFrame( animate );
     controls.update();
     render();
 
   };
-
-  function render() {
+  
+  function render () {
 
     camera.lookAt( scene.position );
+    tweets.rotation.y +=0.001;
     earth.rotation.y -= 0.005;
     renderer.render( scene, camera );
     stats.update();
--- js/src/gt.utils.js
+++ js/src/gt.utils.js
@@ -1,11 +1,40 @@
-// convert the positions from a lat, lon to a position on a sphere.
-function latLongToVector3(lat, lon, radius, heigth) {
-    var phi = (lat)*Math.PI/180;
-    var theta = (lon-180)*Math.PI/180;
+gt.utils = {
 
-    var x = -(radius+heigth) * Math.cos(phi) * Math.cos(theta);
-    var y = (radius+heigth) * Math.sin(phi);
-    var z = (radius+heigth) * Math.cos(phi) * Math.sin(theta);
+	latLongToVector3: function  (lat, lon, radius, heigth) {
+	    var phi = (lat)*Math.PI/180;
+	    var theta = (lon-180)*Math.PI/180;
 
-    return new THREE.Vector3(x,y,z);
-}
+	    var x = -(radius+heigth) * Math.cos(phi) * Math.cos(theta);
+	    var y = (radius+heigth) * Math.sin(phi);
+	    var z = (radius+heigth) * Math.cos(phi) * Math.sin(theta);
+
+	    return new THREE.Vector3(x,y,z);
+	},
+
+	addPoints: function (data) {
+
+	    
+	    var particles = new THREE.Geometry();
+
+	    var particleMaterial = new THREE.ParticleBasicMaterial({ color: 0xFFFFFF, size: 200, opacity:0.8 });
+	    for (var i = 0 ; i < data.length-1 ; i++) {
+
+
+	        var x = parseInt(data[i][0])+180;
+	        var y = parseInt((data[i][1])-84)*-1;
+
+
+
+	        var position = this.latLongToVector3(y, x, 600, 2);
+
+
+	        var particle = new THREE.Vector3(position);
+            particles.vertices.push(particle); 
+
+	    }
+
+	    return new THREE.ParticleSystem(particles, particleMaterial);
+
+	    
+	}
+};
