2e0f1aea27de15ba1abf7ffbee3938efa8ab9bd6
Aysegul Yonet <aysegulyonet@gmail.com>
2013-09-30 00:21:15 -0700

    serving files with express, socket io

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ var app          = express();
 var server       = require('http').createServer(app);
 var io           = require('socket.io').listen(server);
 process.env.PORT = process.env.PORT || 8080;
+var stylus       = require('stylus');
 var path         = require('path');
 var url          = 'http://localhost:' + process.env.PORT + '/';
+
+
+
+
+//serve the html page and client files
+app.use(express.static(path.join(__dirname + '/client')));
 
 // listening to process.env.PORT...
 server.listen(process.env.PORT);
 console.log("Express server listening on port " + process.env.PORT);
 console.log(url);
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('data', {});
+	socket.on('send', function (data) {
+		io.sockets.emit('data', data);
+	});
+
+});
